418159853720a888b681fe1e4c4fe6f39be71474
svarcoder <charvai1997@gmail.com>
2022-12-30 13:11:24 +0530

    Final_Code

---
+++ src/temp/EmailData.js
@@ -0,0 +1,26 @@
+export const emailData = [
+	{
+		starred: false,
+		from: "A",
+		subject: "Hello & Welcome",
+		message: "Welcome to my website",
+		received: "08:20",
+		read: false,
+	},
+	{
+		starred: true,
+		from: "B",
+		subject: "Hello & Welcome",
+		message: "Welcome to my website",
+		received: "08:30",
+		read: true,
+	},
+	{
+		starred: true,
+		from: "C",
+		subject: "Hello & Welcome",
+		message: "Welcome to my website",
+		received: "09:25",
+		read: true,
+	},
+];
